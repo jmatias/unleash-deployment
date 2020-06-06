8724154e8a5a85d491fdd13ef12f4b6a0f2bc99e
Javier Matias <jmatias@thoughtworks.com>
2020-06-05 21:34:39 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,18 @@
+.PHONY: build
+
+ifndef DOCKER_TAG
+DOCKER_TAG := "0.2.8"
+endif
+
+all: docker
+
+docker:
+	docker build -t jmatiastw/unleash:$(DOCKER_TAG) .
+
+docker-push: docker
+	docker push jmatiastw/unleash:$(DOCKER_TAG)
+
+push: docker-push
+
+chart:
+	helm push ./deploy/unleash chartmuseum
---
+++ deploy/unleash/serverfiles/server.js
@@ -0,0 +1,63 @@
+const unleash = require('unleash-server');
+const {User} = require('unleash-server/lib/server-impl');
+const auth = require('basic-auth');
+
+
+console.log(`Username: ${process.env.UNLEASH_USERNAME}`);
+console.log(`Database: ${process.env.UNLEASH_DATABASE}`);
+console.log(`postgres://postgres:${process.env.UNLEASH_PASSWORD}@unleash-postgresql:5432/${process.env.UNLEASH_DATABASE}`);
+
+function basicAuthentication(app) {
+    app.use('/api/admin/', (req, res, next) => {
+        const credentials = auth(req);
+        console.log(credentials)
+
+        if (credentials) {
+            // TODO  - fixme
+            if (credentials.pass === 'admin') {
+                // you will need to do some verification of credentials here.
+                const user = new User({email: `${credentials.name}@admin.com`, id: 1, name: 'Admin'});
+                req.user = user;
+                return next();
+            }
+        }
+
+        return res
+            .status('401')
+            .set({'WWW-Authenticate': 'Basic realm="example"'})
+            .end('access denied');
+    });
+
+    app.use((req, res, next) => {
+        // Updates active sessions every hour
+        req.session.nowInHours = Math.floor(Date.now() / 3600e3);
+        next();
+    });
+}
+
+
+unleash
+    .start({
+        databaseUrl: `postgres://postgres:${process.env.UNLEASH_PASSWORD}@unleash-postgresql:5432/${process.env.UNLEASH_DATABASE}`,
+        ui: {
+            headerBackground: `${process.env.HEADER_BACKGROUND_COLOR}`
+        },
+        port: 4242,
+        adminAuthentication: 'none',
+        getLogger: function (name) {
+            // do something with the name
+            return {
+                debug: console.log,
+                info: console.log,
+                warn: console.log,
+                error: console.error,
+            };
+        }
+
+    })
+    .then(unleash => {
+
+        console.log(
+            `Unleash started on http://${process.env.UNLEASH_HOST_URL}`,
+        );
+    });
